7701fd3c25286772d755733f50eed0701b9c28b4
nadialvy <5026231007@student.its.ac.id>
2024-07-02 12:48:01 +0700

    init project

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Nadia Lovely on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
